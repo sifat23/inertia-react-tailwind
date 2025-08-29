819f946ba8a796bb4d59d716a3dbcd3fe04379ff
shifat23 <shifat@revo-interactive.com>
2025-08-30 02:44:20 +0600

    first

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Welcome() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">Hello from Inertia, React, and Tailwind!</h1>
+        </div>
+    );
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import './bootstrap';
+import {createInertiaApp} from "@inertiajs/react";
+import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
+import {createRoot} from "react-dom/client";
+
+createInertiaApp({
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
