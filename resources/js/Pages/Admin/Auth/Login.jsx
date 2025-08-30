f399dab37449a013288e02fa92a3526effef9412
shifat23 <shifat@revo-interactive.com>
2025-08-30 19:12:32 +0600

    login page responsive done

--- resources/js/Pages/Admin/Auth/Login.jsx
+++ resources/js/Pages/Admin/Auth/Login.jsx
@@ -10,21 +10,20 @@ export default function Welcome() {
 
     return (
         <div className="font-[var(--font-primary)] flex flex-col h-screen items-center justify-center bg-background-dark">
-
-            <div className="bg-login-card-body w-xl rounded-md shadow-slow-hide hover:shadow-link-color duration-500 ease-in-out">
-                <div className="text-center mt-12 mb-12">
-                    <h3 className="text-4xl font-bold text-white">Sign In</h3>
-                    <p className="text-white mt-6 text-xl">Access Your Account</p>
+            <div className="bg-login-card-body w-7/8 md:w-xl rounded-md shadow-slow-hide hover:shadow-link-color duration-500 ease-in-out">
+                <div className="text-center mt-4 md:mt-12 mb-6 md:mb-12">
+                    <h3 className="text-xl md:text-4xl font-bold text-white">Sign In</h3>
+                    <p className="text-white mt-2 md:mt-6 text-shadow-md text-base md:text-xl">Access Your Account</p>
                 </div>
 
-                <div id="card" className="mx-14 mb-12">
+                <div id="card" className="mx-4 md:mx-14 mb-12">
                     <input type="text"
-                           className="block w-full p-3.5 h-16 rounded-md border-input-border bg-input-background text-white"
+                           className="block text-sm md:text-base w-full p-3.5 h-10 md:h-16 rounded-md border-input-border bg-input-background text-white"
                            placeholder="Your Email"/>
 
                     <div className="relative mt-5 w-full">
                         <input type={showPass ? "text" : "password"}
-                               className="block mt-5 w-full p-3.5 h-16 rounded-md border-input-border bg-input-background text-white"
+                               className="block mt-5 w-full p-3.5 text-sm md:text-base md:h-16 rounded-md border-input-border bg-input-background text-white"
                                placeholder="Password"/>
                         <BasicButton
                             onClick={() => setShowPass(!showPass)}
@@ -37,32 +36,32 @@ export default function Welcome() {
                     <div className="flex justify-between mt-4">
                         <div className="flex align-middle">
                             <input id="remind_me" type="checkbox"/>
-                            <label htmlFor="remind_me" className="text-white ml-2">Remind me</label>
+                            <label htmlFor="remind_me" className="text-white mt-[2px] md:mt-0 text-sm md:text-base ml-2">Remind me</label>
                         </div>
 
-                        <Link href="/" className="text-white hover:underline">
+                        <Link href="/" className="text-white hover:underline mt-[2px] md:mt-0 text-sm md:text-base">
                             Forget Password?
                         </Link>
                     </div>
 
                     <div className="mt-12 w-full">
-                        <BasicButton className="w-full h-16 text-2xl font-bold duration-300 ease-in-out shadow-slow-hide hover:shadow-link-color bg-linear-[135deg] to-blue-500 from-green-400 hover:to-blue-400 hover:from-green-300">
+                        <BasicButton className="w-full md:h-16 md:text-2xl font-bold duration-300 ease-in-out shadow-slow-hide hover:shadow-link-color bg-linear-[135deg] to-blue-500 from-green-400 hover:to-blue-400 hover:from-green-300">
                             SIGN IN
                         </BasicButton>
                     </div>
 
-                    <div className="mt-6 text-center text-white">
+                    <div className="mt-4 text-sm md:text-base md:mt-6 text-center text-white">
                         Does not have an account yet? <Link className="text-link-color hover:underline">Create and account</Link>
                     </div>
 
-                    <div className="flex items-center w-full mt-12">
+                    <div className="flex items-center w-full mt-6 md:mt-12">
                         <div className="flex-grow border-t border-input-border"></div>
                         <span className="mx-4 text-white">OR</span>
                         <div className="flex-grow border-t border-input-border"></div>
                     </div>
 
-                    <div className="text-white flex justify-center mt-6">
-                        <BasicButton className="flex px-6 items-center border border-login-card-body duration-300 ease-in-out shadow-basic-hide hover:shadow-link-color hover:border hover:border-link-color">
+                    <div className="text-white flex justify-center mt-4 md:mt-6">
+                        <BasicButton className="flex md:px-6 items-center border border-login-card-body duration-300 ease-in-out shadow-basic-hide hover:shadow-link-color hover:border hover:border-link-color">
                             <FcGoogle className="text-3xl" />
                             <div className="ml-2">
                                 Continue with Google
